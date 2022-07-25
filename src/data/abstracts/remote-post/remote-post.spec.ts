c4ed375591197980770b4a096943fbd713a38256
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-25 14:28:05 -0300

    test: return tag mapped on AbstractRemotePost

--- src/data/abstracts/remote-post/remote-post.spec.ts
+++ src/data/abstracts/remote-post/remote-post.spec.ts
@@ -1,8 +1,22 @@
+import { makeRemoteTagMock } from 'data/models/remote-tag/mock'
 import { AbstractRemotePost } from './remote-post'
 
 class Sut extends AbstractRemotePost {}
 
 describe('AbstractRemotePost', () => {
+  it('should return tag mapped', () => {
+    const remoteTagMock = makeRemoteTagMock()
+    const tags = Sut.mapTags([remoteTagMock])
+
+    const remoteTagMapped = {
+      id: remoteTagMock.id,
+      title: remoteTagMock.attributes?.title,
+      slug: remoteTagMock.attributes?.slug,
+    }
+
+    expect(tags).toStrictEqual([remoteTagMapped])
+  })
+
   it('should return empty tag array with receive nothing in params', () => {
     const tags = Sut.mapTags()
 
---
+++ src/data/models/remote-tag/mock.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker'
+import { RemoteTag } from '..'
+
+export const makeRemoteTagMock = (): RemoteTag.Model => ({
+  id: faker.datatype.uuid(),
+  attributes: {
+    title: faker.random.words(),
+    slug: faker.datatype.uuid(),
+  },
+})
