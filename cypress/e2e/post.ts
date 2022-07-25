639459ef7537321f6bc4206e104b9fe904a46786
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-25 13:07:20 -0300

    test: render post page with cypress (close #16)

--- cypress/e2e/post.ts
+++ cypress/e2e/post.ts
@@ -1,15 +1,13 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Post Page', () => {
-  it('should go back to home page', () => {
+  it('should render post page', () => {
     cy.visit('/')
 
-    cy.findAllByRole('article').contains('lorem').click()
-    cy.url().should('include', '@')
+    cy.getByDataCy('last-post').first().click()
 
-    cy.findByText(/fundador test/i).should('exist')
-    cy.findByAltText(/logo nuancias/i).click()
-
-    cy.url().should('eq', 'http://localhost:3000/')
+    cy.findByAltText(/usuário/i).should('exist')
+    cy.findByAltText(/história/i).should('exist')
+    cy.get('#article-content').should('exist')
   })
 })
