4a9eabd1e8b4f23cdc5f7dc03250047a6912379e
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-26 09:42:37 -0300

    feat: created useGetPostsByText hook

---
+++ src/presentation/hooks/use-get-posts-by-text.ts
@@ -0,0 +1,33 @@
+import { SearchedPost } from 'domain/models'
+import { SearchPosts } from 'domain/use-cases'
+import { useCallback, useState } from 'react'
+
+export const useGetPostsByText = (searchPosts: SearchPosts) => {
+  const [posts, setPosts] = useState<SearchedPost.Model[] | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const getPosts = useCallback(
+    async (text: string) => {
+      setIsLoading(true)
+      try {
+        const response = await searchPosts.getAllByText(text)
+        setPosts(response)
+      } catch (error) {
+        if (error instanceof Error) {
+          const errorMessage = error.message
+          setError(errorMessage)
+        }
+      }
+      setIsLoading(false)
+    },
+    [searchPosts]
+  )
+
+  return {
+    posts,
+    isLoading,
+    error,
+    getPosts,
+  }
+}
