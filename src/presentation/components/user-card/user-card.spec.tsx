d0dcc83af3f597a9d3a9cd6af90ce5a63d372d49
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-26 15:15:55 -0300

    refactor: commented link to user page because is not ready

--- src/presentation/components/user-card/user-card.spec.tsx
+++ src/presentation/components/user-card/user-card.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { makeUserCommonMock } from 'domain/models/common/user/mock'
-import { makeUserLink } from 'presentation/routers/helpers'
+//import { makeUserLink } from 'presentation/routers/helpers'
 
 import { UserCard } from './user-card'
 
@@ -13,10 +13,10 @@ describe('<UserCard />', () => {
     const userData = makeUserCommonMock()
     makeSut(userData)
 
-    expect(screen.getByRole('link')).toHaveAttribute(
-      'href',
-      makeUserLink(userData.username)
-    )
+    // expect(screen.getByRole('link')).toHaveAttribute(
+    //   'href',
+    //   makeUserLink(userData.username)
+    // )
     expect(screen.getByAltText(/usuário principal/i)).toBeInTheDocument()
     expect(screen.getByText(userData.name)).toBeInTheDocument()
     expect(screen.getByText(userData.bio)).toBeInTheDocument()
--- src/presentation/components/user-card/user-card.tsx
+++ src/presentation/components/user-card/user-card.tsx
@@ -1,7 +1,7 @@
 import { UserModel } from 'domain/models'
 import Image from 'next/image'
-import Link from 'next/link'
-import { makeUserLink } from 'presentation/routers/helpers'
+// import Link from 'next/link'
+// import { makeUserLink } from 'presentation/routers/helpers'
 
 export type UserCardProps = {
   userData: Pick<UserModel, 'username' | 'name' | 'bio' | 'avatar'>
@@ -9,23 +9,23 @@ export type UserCardProps = {
 
 export const UserCard = ({ userData }: UserCardProps) => {
   return (
-    <Link href={makeUserLink(userData.username)}>
-      <a className="flex cursor-pointer">
-        <figure className="flex mr-4">
-          <Image
-            className="rounded-full"
-            src={userData.avatar.src}
-            width={40}
-            height={40}
-            objectFit="cover"
-            alt="Imagem do usuário principal"
-          />
-        </figure>
-        <div className="flex flex-col">
-          <span className="font-semibold">{userData.name}</span>
-          <span className="text-xs text-gray-500">{userData.bio}</span>
-        </div>
-      </a>
-    </Link>
+    //<Link href={makeUserLink(userData.username)}>
+    <a className="flex cursor-default">
+      <figure className="flex mr-4">
+        <Image
+          className="rounded-full"
+          src={userData.avatar.src}
+          width={40}
+          height={40}
+          objectFit="cover"
+          alt="Imagem do usuário principal"
+        />
+      </figure>
+      <div className="flex flex-col">
+        <span className="font-semibold">{userData.name}</span>
+        <span className="text-xs text-gray-500">{userData.bio}</span>
+      </div>
+    </a>
+    //</Link>
   )
 }
