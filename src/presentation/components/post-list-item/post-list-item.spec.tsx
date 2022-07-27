f73068b92af721d685458a75b5fffd645d4ac12b
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-27 13:12:15 -0300

    test: add PostListItem component

---
+++ src/presentation/components/post-list-item/post-list-item.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+import { makePostCardsMock } from 'domain/models/post/mock'
+import { makePtBRDate } from '../post-card/helpers'
+
+import PostListItem from './post-list-item'
+
+const makeSut = (fakePost = makePostCardsMock()[0]) => {
+  render(<PostListItem post={fakePost} />)
+}
+
+describe('<PostListItem />', () => {
+  it('should render with props', () => {
+    const fakePost = makePostCardsMock()[0]
+    makeSut(fakePost)
+
+    expect(screen.getByAltText(/Imagem do usuário/i)).toBeInTheDocument()
+    expect(screen.getByText(fakePost.writer.name)).toBeInTheDocument()
+    expect(screen.getByText(fakePost.title)).toBeInTheDocument()
+    expect(
+      screen.getByText(makePtBRDate(fakePost.publishedAt))
+    ).toBeInTheDocument()
+    expect(screen.getByAltText(/Imagem da história/i)).toBeInTheDocument()
+
+    fakePost.tags.forEach((tag) => {
+      expect(screen.getByText(tag.title)).toBeInTheDocument()
+    })
+  })
+})
