28fb47038130645a8b7ec0ffb381c821bd3526ac
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-29 17:26:44 -0300

    feat: add types for RemoteWriterPageData

--- src/data/models/writer/remote-writer-page-data/remote-writer-page-data.ts
+++ src/data/models/writer/remote-writer-page-data/remote-writer-page-data.ts
@@ -4,8 +4,14 @@ import {
 } from 'data/protocols/http/graphql/types'
 
 export type QueryResponse = GetUserAndPostsQuery
-export type UserData = NonNullable<
+export type WriterData = NonNullable<
   QueryResponse['usersPermissionsUsers']
+>['data'][0]
+export type PostData = NonNullable<
+  NonNullable<WriterData['attributes']>['posts']
+>['data'][0]
+export type TagData = NonNullable<
+  NonNullable<PostData['attributes']>['tags']
 >['data'][0]
 
 export type QueryVariables = GetUserAndPostsQueryVariables
