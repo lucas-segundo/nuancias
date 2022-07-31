5fd2182047d76b741eb56fc3bcf5831a7ae5a93b
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-07-31 09:59:51 -0300

    refactor: add missing fake data in PostTag story

--- src/presentation/components/post-tag/post-tag.stories.tsx
+++ src/presentation/components/post-tag/post-tag.stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react'
+import { makePostCardTagMock } from 'domain/models/post/post-card/mock'
 import { PostTag, PostTagProps } from './post-tag'
 
 export default {
@@ -7,3 +8,7 @@ export default {
 } as Meta
 
 export const Default: Story<PostTagProps> = (args) => <PostTag {...args} />
+
+Default.args = {
+  tagData: makePostCardTagMock(),
+}
