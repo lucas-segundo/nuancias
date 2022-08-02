02d854e9f12ccf970e8e6a94c973d4f2ed13e4ef
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-02 09:17:24 -0300

    refactor: getBySlug have to return model or error

--- src/domain/use-cases/post/load-post-content-data.ts
+++ src/domain/use-cases/post/load-post-content-data.ts
@@ -1,5 +1,5 @@
 import { PostContentModel } from 'domain/models'
 
 export interface LoadPostContentData {
-  getBySlug(slug: string): Promise<PostContentModel.Model | null>
+  getBySlug(slug: string): Promise<PostContentModel.Model>
 }
