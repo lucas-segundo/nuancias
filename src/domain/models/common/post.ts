a64bef096ba4922c1a84993fa2e9a9d7981b2aca
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-02 17:24:16 -0300

    feat: add generic for image type

--- src/domain/models/common/post.ts
+++ src/domain/models/common/post.ts
@@ -1,8 +1,11 @@
-export type PostModel = {
+import { ImageModel } from './image'
+
+export type PostModel<Image = ImageModel> = {
   id: string
   title: string
   slug: string
   preview: string
   content: string
   publishedAt: string
+  image: Image
 }
