ce2cc88ea6c273684b4c0e0c67e1af7aa26c3b73
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-03 09:47:25 -0300

    feat: return data if query is success

--- src/data/use-cases/remote-load-tag-details/remote-load-tag-details.ts
+++ src/data/use-cases/remote-load-tag-details/remote-load-tag-details.ts
@@ -1,10 +1,12 @@
-import { AbstractAuthToken } from 'data/abstracts'
+import { AbstractRemoteTag } from 'data/abstracts/remote-tag/remote-tag'
+import { IMAGE_PLACEHOLDER } from 'data/helpers'
 import { RemoteTagDetails } from 'data/models'
 import { GraphqlClient } from 'data/protocols/http'
+import { TagDetailsModel } from 'domain/models'
 import { LoadTagDetails } from 'domain/use-cases'
 
 export class RemoteLoadTagDetails
-  extends AbstractAuthToken
+  extends AbstractRemoteTag
   implements LoadTagDetails.Model
 {
   constructor(
@@ -15,7 +17,7 @@ export class RemoteLoadTagDetails
   }
 
   async get(params: LoadTagDetails.Params) {
-    await this.graphqlClient.query<
+    const response = await this.graphqlClient.query<
       RemoteTagDetails.QueryVariables,
       RemoteTagDetails.QueryResponse
     >({
@@ -26,5 +28,66 @@ export class RemoteLoadTagDetails
         cachePolicy: 'no-cache',
       },
     })
+
+    return this.adaptResponseToModel(response.data)
+  }
+
+  adaptResponseToModel(
+    data: RemoteTagDetails.QueryResponse
+  ): TagDetailsModel.Model | undefined {
+    const tag = data.tag?.data
+
+    if (tag) return this.mapValidTag(tag)
+  }
+
+  private mapValidTag(
+    tag: RemoteTagDetails.TagData
+  ): TagDetailsModel.Model | undefined {
+    const tagAttr = tag?.attributes
+
+    const validPosts = tagAttr?.posts?.data.reduce<TagDetailsModel.Post[]>(
+      (validPosts, post) => {
+        const result = this.mapValidPost(post)
+        result && validPosts.push(result)
+
+        return validPosts
+      },
+      []
+    )
+
+    if (validPosts && tagAttr && tag.id)
+      return {
+        id: tag.id,
+        title: tagAttr.title,
+        slug: tagAttr.slug,
+        posts: validPosts,
+      }
+  }
+
+  private mapValidPost(
+    post: RemoteTagDetails.PostData
+  ): TagDetailsModel.Post | undefined {
+    const postAttr = post.attributes
+
+    const postUrl = this.getImageFormat(
+      postAttr?.image.data?.attributes?.formats,
+      IMAGE_PLACEHOLDER.POST
+    )
+
+    if (post.id && postAttr) {
+      const preview = this.makePreview(postAttr.content)
+      const { title, slug, publishedAt } = postAttr
+
+      return {
+        id: post.id,
+        title,
+        slug,
+        preview,
+        publishedAt,
+        image: {
+          src: postUrl,
+        },
+      }
+    }
   }
 }
