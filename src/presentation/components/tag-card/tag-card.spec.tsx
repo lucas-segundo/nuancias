aa44a43cc8262776d3e775628bb241a7c4d7b6c0
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-04 14:24:13 -0300

    refactor: change test description

--- src/presentation/components/tag-card/tag-card.spec.tsx
+++ src/presentation/components/tag-card/tag-card.spec.tsx
@@ -4,7 +4,7 @@ import { makeTagPreview } from 'domain/models/tag/preview/mock'
 import { TagCard } from './tag-card'
 
 describe('<TagCard />', () => {
-  it('should render the heading', () => {
+  it('should render correctly', () => {
     const fakeTag = makeTagPreview()
     render(<TagCard tag={fakeTag} />)
 
