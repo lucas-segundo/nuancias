22ac5cc8bc2e7473d962fc28bbd5309a8081bfbe
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-04 15:13:08 -0300

    fix: created makeLoadTagPreview return class

--- src/main/factories/use-cases/load-tag-preview/load-tag-preview-factory.ts
+++ src/main/factories/use-cases/load-tag-preview/load-tag-preview-factory.ts
@@ -1,9 +1,9 @@
-import { RemoteLoadTagDetails } from 'data/use-cases'
+import { RemoteLoadTagPreview } from 'data/use-cases/remote-load-tag-preview/remote-load-tag-preview'
 import { ApolloGraphqlClient } from 'infra/http'
 import { GET_TAGS } from 'infra/http/apollo-client/operations/queries'
 
 export const makeLoadTagPreview = () => {
-  return new RemoteLoadTagDetails(
+  return new RemoteLoadTagPreview(
     new ApolloGraphqlClient(`${process.env.NEXT_PUBLIC_CMS_API_URL}/graphql`),
     GET_TAGS
   )
