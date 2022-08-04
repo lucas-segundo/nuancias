a819fbddb998535088e7f16e81c5cba343ba93d9
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-04 10:08:56 -0300

    feat: add types for RemoteTagPreview

--- src/data/models/tag/remote-tag-preview/remote-tag-preview.ts
+++ src/data/models/tag/remote-tag-preview/remote-tag-preview.ts
@@ -1,4 +1,12 @@
+import {
+  GetTagsQuery,
+  GetTagsQueryVariables,
+} from 'data/protocols/http/graphql/types'
+
 export type Model = {
   id?: string | null
   attributes?: { title: string; slug: string } | null
 }
+
+export type QueryVariables = GetTagsQueryVariables
+export type QueryResponse = GetTagsQuery
