807445240c987d2549ac3a577c12a45eaed823af
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-04 17:20:28 -0300

    feat: add meta tag

--- src/presentation/templates/tag-posts/tag-posts.tsx
+++ src/presentation/templates/tag-posts/tag-posts.tsx
@@ -1,6 +1,7 @@
 import { TagDetailsModel } from 'domain/models'
 import { SearchPosts } from 'domain/use-cases'
 import { PostCard } from 'presentation/components'
+import { Meta } from 'presentation/components/elements'
 import { TagCard } from 'presentation/components/tag-card/tag-card'
 import { Base } from 'presentation/layouts'
 
@@ -22,20 +23,26 @@ const renderPostCards = (posts: TagDetailsModel.Post[]) => {
 
 export const TagPosts = (props: TagPostsProps) => {
   return (
-    <Base searchPosts={props.searchPosts}>
-      <div className="default-screen-margin mt-10 mb-80">
-        <div className="my-7">
-          <TagCard tag={props.tag} />
-          <h1 className="text-xl lg:text-2xl font-bold">Histórias</h1>
-          <div className="space-y-1">
-            <span className="bg-cyan-400 w-10 lg:w-20 h-0.5 block"></span>
-            <span className="bg-fuchsia-400 w-5 lg:w-10 h-0.5 block"></span>
+    <>
+      <Meta
+        title={`Nuancias - ${props.tag.title}`}
+        description="Veja as histórias relacionado a essa nuancia."
+      />
+      <Base searchPosts={props.searchPosts}>
+        <div className="default-screen-margin mt-10 mb-80">
+          <div className="my-7">
+            <TagCard tag={props.tag} />
+            <h1 className="text-xl lg:text-2xl font-bold">Histórias</h1>
+            <div className="space-y-1">
+              <span className="bg-cyan-400 w-10 lg:w-20 h-0.5 block"></span>
+              <span className="bg-fuchsia-400 w-5 lg:w-10 h-0.5 block"></span>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
+            {renderPostCards(props.tag.posts)}
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
-          {renderPostCards(props.tag.posts)}
-        </div>
-      </div>
-    </Base>
+      </Base>
+    </>
   )
 }
