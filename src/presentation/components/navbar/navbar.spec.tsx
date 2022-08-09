a38356578e08c0a29c8352e124b4a323ffb2b237
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-09 10:21:04 -0300

    test: render correctly and navegation items for NavItems

--- src/presentation/components/navbar/navbar.spec.tsx
+++ src/presentation/components/navbar/navbar.spec.tsx
@@ -1,13 +1,38 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { SearchPosts } from 'domain/use-cases'
 import { Navbar } from './navbar'
+import { checkIsMobileScreen } from 'presentation/helpers'
+
+jest.mock('presentation/helpers')
 
 const searchPostsMocked: jest.Mocked<SearchPosts> = {
   getAllByText: jest.fn(),
 }
 
+const makeSut = () => {
+  render(<Navbar searchPosts={searchPostsMocked} />)
+}
+
 describe('<Navbar />', () => {
   it('should render correctly', () => {
-    render(<Navbar searchPosts={searchPostsMocked} />)
+    makeSut()
+  })
+
+  it('should not hide navegation items in desktop', () => {
+    const checkIsMobileScreenMocked = checkIsMobileScreen as jest.Mock
+    checkIsMobileScreenMocked.mockReturnValue(false)
+
+    makeSut()
+
+    expect(screen.getByRole('navbar-items')).toBeVisible()
+  })
+
+  it('should hide navegation items in mobile', () => {
+    const checkIsMobileScreenMocked = checkIsMobileScreen as jest.Mock
+    checkIsMobileScreenMocked.mockReturnValue(true)
+
+    makeSut()
+
+    expect(screen.queryByRole('navbar-items')).toBeNull()
   })
 })
