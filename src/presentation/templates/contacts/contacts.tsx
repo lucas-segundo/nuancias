d075e6b262a4aa09c3f43d07603d51f9275fdd67
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-10 13:59:07 -0300

    fix: add missing metatags (close #40)

--- src/presentation/templates/contacts/contacts.tsx
+++ src/presentation/templates/contacts/contacts.tsx
@@ -1,4 +1,5 @@
 import { NavbarProps } from 'presentation/components'
+import { Meta } from 'presentation/components/elements'
 import { Base } from 'presentation/layouts'
 import {
   AiFillLinkedin,
@@ -10,42 +11,48 @@ export type ContactsProps = NavbarProps
 
 export const Contacts = (props: ContactsProps) => {
   return (
-    <Base searchPosts={props.searchPosts}>
-      <div className="default-screen-margin border mt-10 rounded p-6">
-        <h1 className="text-4xl font-bold">Contatos</h1>
-        <p className="my-2 text-lg">
-          Escolha a melhor forma de falar conosco:{' '}
-        </p>
-        <ul className="text-lg">
-          <li className="flex items-center">
-            <AiOutlineMail />
-            <a
-              className="underline ml-1"
-              href="mailto: lucas.fernandes.app@gmail.com"
-            >
-              Email: lucas.fernandes.app@gmail.com
-            </a>
-          </li>
-          <li className="flex items-center">
-            <AiFillLinkedin />
-            <a
-              className="underline ml-1"
-              href="https://www.linkedin.com/in/engplucasfernandes/"
-            >
-              Linkedin: engplucasfernandes
-            </a>
-          </li>
-          <li className="flex items-center">
-            <AiOutlineInstagram />
-            <a
-              className="underline"
-              href="https://www.instagram.com/lucassegundof/"
-            >
-              Instagram: lucassegundof
-            </a>
-          </li>
-        </ul>
-      </div>
-    </Base>
+    <>
+      <Meta
+        title="Nuancias - Contatos"
+        description="Veja as melhores formas de falar conosco."
+      />
+      <Base searchPosts={props.searchPosts}>
+        <div className="default-screen-margin border mt-10 rounded p-6">
+          <h1 className="text-4xl font-bold">Contatos</h1>
+          <p className="my-2 text-lg">
+            Escolha a melhor forma de falar conosco:{' '}
+          </p>
+          <ul className="text-lg">
+            <li className="flex items-center">
+              <AiOutlineMail />
+              <a
+                className="underline ml-1"
+                href="mailto: lucas.fernandes.app@gmail.com"
+              >
+                Email: lucas.fernandes.app@gmail.com
+              </a>
+            </li>
+            <li className="flex items-center">
+              <AiFillLinkedin />
+              <a
+                className="underline ml-1"
+                href="https://www.linkedin.com/in/engplucasfernandes/"
+              >
+                Linkedin: engplucasfernandes
+              </a>
+            </li>
+            <li className="flex items-center">
+              <AiOutlineInstagram />
+              <a
+                className="underline"
+                href="https://www.instagram.com/lucassegundof/"
+              >
+                Instagram: lucassegundof
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Base>
+    </>
   )
 }
