7685991be12940dbf87a7673fe89cf8fc70f2d74
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2022-08-11 09:04:13 -0300

    feat: add Article JSON-LD

--- src/presentation/templates/post/post.tsx
+++ src/presentation/templates/post/post.tsx
@@ -7,6 +7,8 @@ import {
   WriterCardProps,
 } from 'presentation/components'
 import { Base } from 'presentation/layouts'
+import { ArticleJsonLd } from 'next-seo'
+import { makePostLink, makeWriterLink } from 'presentation/routers/helpers'
 
 export type PostProps = NavbarProps & WriterCardProps & PostContentProps
 
@@ -16,6 +18,28 @@ export const Post = (props: PostProps) => {
       <Meta
         title={`${props.postData.title} - por ${props.writer.name} - Nuancias`}
         description={props.postData.preview}
+      />
+      <ArticleJsonLd
+        url={
+          process.env.NEXT_PUBLIC_SITE_URL +
+          makePostLink(props.writer.username, props.postData.slug)
+        }
+        title={props.postData.title}
+        description={props.postData.preview}
+        images={[props.postData.image.src]}
+        datePublished={props.postData.publishedAt}
+        authorName={[
+          {
+            name: props.writer.name,
+            url:
+              process.env.NEXT_PUBLIC_SITE_URL +
+              makeWriterLink(props.writer.username),
+          },
+        ]}
+        publisherName="Nuancias"
+        publisherLogo={
+          process.env.NEXT_PUBLIC_SITE_URL + '/images/meta-tag-image-logo.jpg'
+        }
       />
       <Base searchPosts={props.searchPosts}>
         <div className="default-screen-margin-post mt-10 mb-80">
